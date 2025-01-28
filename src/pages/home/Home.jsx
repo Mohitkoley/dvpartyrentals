b8a7d59fbc2141b9ccfa7352be16e2b6de75b15a
sayakmallick4875 <sayak@solivatech.com>
2025-01-28 10:43:13 +0530

    Initialize project with React, Vite, and Tailwind CSS setup

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react';
+import HomeIntro from '../../components/HomePage/HomeIntro/HomeIntro';
+import AboutUs from '../../components/HomePage/AboutUs/AboutUs';
+import Services from '../../components/HomePage/Services/Services';
+import Reviews from '../../components/HomePage/Reviews/Reviews';
+import Footer from '../../components/Footer/Footer';
+
+
+const Home = () => {
+  const aboutUsRef = useRef(null);
+  const servicesRef = useRef(null);
+  const homeIntroRef = useRef(null);
+
+  const scrollToSection = (ref) => {
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <div className="bg-custom-gradient w-screen min-h-screen overflow-hidden">
+      <div ref={homeIntroRef}>
+      <HomeIntro />
+      </div>
+       <div ref={aboutUsRef}>
+        <AboutUs />
+      </div>
+      <div ref={servicesRef}>
+        <Services />
+      </div>
+      <Reviews />
+      <Footer scrollToSection={scrollToSection} aboutUsRef={aboutUsRef} servicesRef={servicesRef} homeIntroRef={homeIntroRef} />
+    </div>
+  );
+};
+
+export default Home;
